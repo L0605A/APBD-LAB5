71676352d7ccd35c8615705676a2dac5578fb448
s27888 <l0605@pjwstk.edu.pl>
2024-04-09 15:21:19 +0200

    added code

---
+++ WebApplication1/WebApplication1/Controllers/AnimalsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("/animals-controller")]
+//[Route("[controller]")]
+
+public class AnimalsController : ControllerBase
+{
+    
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        return Ok();
+    }
+    
+    
+    [HttpGet("{id")]
+    public IActionResult GetAnimals(int id)
+    {
+        return Ok(id);
+    }
+    
+    [HttpPost]
+    public IActionResult AddAnimal()
+    {
+        return Created();
+    }
+    
+    [HttpPut("{id")]
+    public IActionResult EditAnimal()
+    {
+        return Ok();
+    }
+    
+    [HttpDelete("{id")]
+    public IActionResult DeleteAnimal()
+    {
+        return Ok();
+    }
+    
+    
+}
---
+++ WebApplication1/WebApplication1/Endpoints/AnimalEndpoints.cs
@@ -0,0 +1,49 @@
+﻿using WebApplication1.Database;
+using WebApplication1.Model;
+
+namespace WebApplication1.Endpoints;
+
+public static class AnimalEndpoints
+{
+    public static void MapAnimalEndpoints(this WebApplication app)
+    {
+        app.MapGet("/animals" , () =>
+        {
+            var animal = StaticData.animals;
+            return Results.Ok(animal);
+        });
+
+        app.MapGet("/animals/{id}" , (int id) =>
+        {
+            foreach(var ele in StaticData.animals)
+            {
+                if (ele.Id == id)
+                {
+                    var animal = ele;
+                    return Results.Ok(animal);
+                }
+            }
+
+            return Results.NotFound();
+        });
+
+        app.MapPost("/animals" , (Animal animal) =>
+        {
+            
+            return Results.Created("", animal);
+        });
+
+        app.MapPut("/animals/{id}" , (int id) =>
+        {
+
+            return Results.Ok();
+        });
+
+
+        app.MapDelete("/animals/{id}" , (int id) =>
+        {
+
+            return Results.Ok();
+        });
+    }
+}
